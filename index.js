f9c997192572fabac20ce34608460529d5114191
devcaballero <claudiocaballero.dev@gmail.com>
2023-08-15 13:30:31 -0300

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const port = 3000; // Choose a suitable port
 
 app.get('/dolaroficial', async (req, res) => {
